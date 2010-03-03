7734610c645f6ddefd0b893ee34abb10458be9e0
Matt Ryall <matt@mattryall.net>
2010-03-03 22:24:01 +0000

    Formatting

--- src/dreme/Substitution.java
+++ src/dreme/Substitution.java
@@ -4,17 +4,19 @@ package dreme;
  * An operator that replaces itself with another SchemeObject to be executed instead.
  */
 public abstract class Substitution extends BasicSchemeObject implements Operator {
+    
     public final void apply(ExecutionContext context) {
-	List substituteList = getSubstitute(context);
-	if (substituteList == null)
-	    context.returnValue(Unspecified.INSTANCE);
-	else
-	    context.executeInPlace(substituteList, getNewEnvironment(context));
+        List substituteList = getSubstitute(context);
+        if (substituteList == null) {
+            context.returnValue(Unspecified.INSTANCE);
+        }
+        else {
+            context.executeInPlace(substituteList, getNewEnvironment(context));
+        }
     }
 
     /**
-     * Default implementation returns the current context. Implementations should override
-     * this to provide a new environment if necessary.
+     * Default implementation returns the current context. Implementations should override this to provide a new environment if necessary.
      */
     protected Environment getNewEnvironment(ExecutionContext context) {
         return context.getEnvironment();
